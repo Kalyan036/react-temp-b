536134502a8d184d35542ef4b5f93a7307abda21
Kalyan036 <padamkalyan80@gmail.com>
2025-03-21 14:48:31 +0530

    updated

--- src/components/App8.jsx
+++ src/components/App8.jsx
@@ -4,9 +4,18 @@ import {useState} from "react";
 export default function App8() {
     const [amount, setAmount] = useState([]);
     const [value, setValue] =useState();
+    const [total, setTotal] = useState(0);
+
     const handleAdd = () =>{
         setAmount([...amount, value]);
     };
+    useEffect(() => {
+      setTotal(
+        amount.reduce((s, value) => {
+          return s + Number(value);
+        }, 0)
+      )
+    }, [amount]);
   return (
     <div>
         <p><input type='number' placeholder='Enter amount' 
@@ -16,7 +25,7 @@ export default function App8() {
         {amount && amount.map(value =>(
             <div>{value}</div>
         ))}
-        Total: {amount.reduce((a,b) => parseInt(a) + parseInt(b), 0)}
+        Total: {total}
     </div>
   )
 }
